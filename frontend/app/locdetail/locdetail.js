5efd5c66db157487d946520a90a8410c43dfeedf
Jonathon Carlson <jon@jc2dev.com>
2015-09-02 21:29:06 -0600

    Todo lists can be added, cleaned up the nav bar a bit, still need to decide on navbar final look and add edit/delete functions for todo lists

--- frontend/app/locdetail/locdetail.js
+++ frontend/app/locdetail/locdetail.js
@@ -8,7 +8,7 @@ angular.module('checkin.locDetail', ['ngRoute', 'ngResource'])
         controller: 'LocationDetailController'
     })
 }])
-.controller('LocationDetailController', ['$scope', '$routeParams', 'Location', '$http', '$location', function($scope, $routeParams, Location, $http, $location){
+.controller('LocationDetailController', ['$scope', '$routeParams', 'Location', '$http', '$location', function($scope, $routeParams, Location, $http, $location, $attrs, $observe){
     Location.get({locationId: $routeParams.locationId}, function(response){
         $scope.location = response
         $scope.title = $scope.location.title
@@ -35,6 +35,26 @@ angular.module('checkin.locDetail', ['ngRoute', 'ngResource'])
     $scope.reset = function(){
         $scope.todo = null
     }
+    // TODO - Get this function working. This might be better in a different view....
+    // $scope.change = function(){
+    //     var id = $(this).attr('data-id')
+    //     console.log(id)
+    //     $http.post(backendUrl + '/todo/' + id, $scope.completed)
+    //     .then(function(response){
+    //         $scope.changed = response.status
+    //         if ($scope.completed === false){
+    //             $scope.message = "Your item has been marked completed"
+    //         } else{
+    //             $scope.message = "Your item is not completed"
+    //         }
+    //     }),
+    //     function(response){
+    //         $scope.errors = response.status
+    //     }
+    // }
+    // $scope.edit = function(){
+        // TODO - either write this function here or put it in a separate view
+    // }
 }])
 .directive('locdetail', function(){
     return {
