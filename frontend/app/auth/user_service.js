36f1e628d1751d923296d6b6e2ba4ce0a1daf332
Jonathon Carlson <jon@jc2dev.com>
2015-09-22 15:36:24 -0600

    Can manually add a new family member from a list of users without families. Also Implemented a family service to reduce server calls

--- frontend/app/auth/user_service.js
+++ frontend/app/auth/user_service.js
@@ -5,6 +5,7 @@ angular.module('checkin.userAuth')
 .service('User', ['$http', '$rootScope', '$location', '$window', function($http, $rootScope, $location, $window){
     var user = {};
     user.info = {};
+    user.family = {};
 
     user.registration = function(user_info){
         return $http.post(backendUrl + '/register-user/', user_info)
@@ -24,6 +25,8 @@ angular.module('checkin.userAuth')
         return $http.get(backendUrl + '/get-user-info/').then(function(data){
             user.info = data.data;
             $rootScope.$broadcast(user.update_broadcast);
+            return user.getFamily();
+
         })
     };
     user.logout = function(){
@@ -53,7 +56,14 @@ angular.module('checkin.userAuth')
         }, {scope: 'public_profile,email'})
     }
 
-    // Consider adding family, locations or check-ins to the User service
+    user.getFamily = function(){
+        $http.get(backendUrl + '/families/' + user.info.info.family + '/').then(function(data){
+            user.family = data.data
+        })
+    }
+
+    // Consider adding check-ins to the User service
+
 
     user.token_name = 'auth-token';
     user.update_broadcast = 'user-updated';
